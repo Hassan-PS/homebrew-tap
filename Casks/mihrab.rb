a16b58946d295767aacca98cf48cc35825ea8ffb
Hassan El Ghamri <hassan15015099@gmail.com>
2026-09-20 20:54:30 +0200

    mihrab 2.24.1

--- Casks/mihrab.rb
+++ Casks/mihrab.rb
@@ -1,6 +1,6 @@
 cask "mihrab" do
-  version "2.24.0"
-  sha256 "083583f4bb563d0e0a644758494165821ef960079d92a980e2b7dbc8d1a21796"
+  version "2.24.1"
+  sha256 "66596adcc37fa47e11e46d14893a7df1bb969497bad571c3035d0c944f5cc8c9"
 
   url "https://github.com/Hassan-PS/Mihrab/releases/download/v#{version}/Mihrab-macOS-#{version}.zip"
   name "Mihrab"
